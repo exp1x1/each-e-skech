0b89cb068a02e23f0a135227c9d1c8c153b6895a
exp1x1 <gorvkrtianliya@gmail.com>
2022-08-07 19:35:42 +0530

    made complete div

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
+const button = document.querySelector(".butt");
 
+const div = document.createElement("div");
+div.classList.add("box");
+div.style.backgroundColor = "grey";
+div.style.width = "25px";
+div.style.height = "25px";
+div.innerText = "1";
 
-const div = document.createElement('div');
-div.classList.add('box');
-div.style.backgroundColor = 'grey';
-div.style.width = '25px';
-div.style.height = '25px';
-div.innerText = '1';
+let boxNumber = 256;
 
-for (let i = 1; i <= 64; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    div.innerText = i;
-    
-    container.appendChild(div);
+createDiv();
+
+button.addEventListener("click", () => {
+    boxnum = prompt("enter the box number: ", "");
+    boxNumber = boxnum * boxnum;
+  removeDivChild();
+  createDiv(getSize(boxNumber),getSize(boxNumber));
+  console.log('box number is :' ,boxNumber);
+  console.log('box sixe is :' ,getSize(boxNumber));
+});
+
+function getSize(size){
+   let eachcell = 640000 / size;
+   return Math.round(Math.sqrt(eachcell)); 
 }
 
-const box = document.querySelectorAll('.box');
 
-box.forEach(box => box.addEventListener("mouseover",  changeColor ));
- 
+function removeDivChild(){
+    const list = document.querySelectorAll('.box');
+    list.forEach(list => {list.remove()})
+}
 
-function changeColor(){
-    console.log(this)
-    this.style.backgroundColor = 'grey';
+function createDiv(h = 50,w = 50) {
+  for (let i = 1; i <= boxNumber; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    div.style.height = `${h}px`;
+    div.style.width = `${w}px`;
+
+    container.appendChild(div);
+  }
+
+  const box = document.querySelectorAll(".box");
+
+  box.forEach((box) => box.addEventListener("mouseover", changeColor));
+}
+function changeColor() {
+  this.style.backgroundColor = `#${randomColor()}`;
+}
+
+function randomColor() {
+  return Math.floor(Math.random() * 0xffffff).toString(16);
 }
